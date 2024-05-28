a168ceb4701e230ee44640376139e18395084bd5
hawoodrose <156238630+hawoodrose@users.noreply.github.com>
2024-05-28 13:19:03 +0200

    mise a jour contact

--- src/app/components/contact-add/contact-add.component.ts
+++ src/app/components/contact-add/contact-add.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WalletManagementService } from '../../services/wallet-management.service';
 import { ContactService, ContactInterface } from '../../services/contact.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule, AlertController } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-contact-add',
   templateUrl: './contact-add.component.html',
   styleUrls: ['./contact-add.component.scss'],
-  imports: [FormsModule, CommonModule, IonicModule],
+  imports: [FormsModule, CommonModule, IonicModule, ReactiveFormsModule],
   standalone: true,
 })
 export class ContactAddComponent implements OnInit {
@@ -29,13 +29,29 @@ export class ContactAddComponent implements OnInit {
     private contactService: ContactService,
     private alertController: AlertController,
     private router: Router
-  ) { }
+  ) {
+    // Écouter les événements de navigation
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.resetForm(); // Réinitialiser le formulaire à chaque navigation vers cette route
+      }
+    });
+  }
+
+
 
   startChat(contactAddress: string): void {
     this.router.navigate(['/talking-page', { address: contactAddress }]);
   }
 
   async ngOnInit() {
+
+    // Réinitialiser les champs du formulaire
+    this.resetForm();
+
+    // Charger les contacts existants
+    this.contacts = this.contactService.getContacts();
+
     // Tentative de connexion au wallet
     const wallet = await this.walletService.connectWithExistingAccount();
     if (wallet) {
@@ -43,45 +59,81 @@ export class ContactAddComponent implements OnInit {
     } else {
       console.error('Failed to connect to wallet');
     }
+  }
 
+  resetForm(): void {
+    this.newContactName = '';
+    this.newContactAddress = '';
+    this.newContactBtcWallet = '';
+    this.newContactEthWallet = '';
+    this.newContactUsdtWallet = '';
+    this.newContactBnbWallet = '';
+    this.newContactSolWallet = '';
+    this.newContactNote = '';
   }
 
   async addContact(): Promise<void> {
     console.log('test');
-    // if (!this.newContactName || !this.newContactAddress) {
-    //   console.log('Missing required fields');
-    //   const alert = await this.alertController.create({
-    //     header: 'Champs obligatoires',
-    //     message: 'Le nom et l\'adresse du contact sont obligatoires.',
-    //     buttons: ['OK']
-    //   });
-    //   await alert.present();
-    //   return;
-    // }
 
-    // const newContact: ContactInterface = {
-    //   id: Date.now().toString(), // Génération d'un identifiant unique pour le contact
-    //   name: this.newContactName,
-    //   address: this.newContactAddress,
-    //   btcadress: this.newContactBtcWallet,
-    //   ethadress: this.newContactEthWallet,
-    //   usdtadress: this.newContactUsdtWallet,
-    //   bnbadress: this.newContactBnbWallet,
-    //   soladress: this.newContactSolWallet,
-    //   note: this.newContactNote
-    // };
-    // this.contactService.addContact(newContact);
-    // this.newContactName = '';
-    // this.newContactAddress = '';
-    // this.newContactBtcWallet = '';
-    // this.newContactEthWallet = '';
-    // this.newContactUsdtWallet = '';
-    // this.newContactBnbWallet = '';
-    // this.newContactSolWallet = '';
-    // this.newContactNote = '';
+    const sanitizedNewContactName = this.newContactName.trim().toLowerCase();
+    const sanitizedNewContactAddress = this.newContactAddress.trim().toLowerCase();
 
-    // // Redirection vers la page des contacts lorsque le contact est ajouté
-    // this.router.navigate(['/contacts-page']);
+    // Vérifier si le nom ou l'adresse du contact existent déjà
+    const nameExists = this.contacts.some(contact => contact.name.trim().toLowerCase() === sanitizedNewContactName);
+    const addressExists = this.contacts.some(contact => contact.address.trim().toLowerCase() === sanitizedNewContactAddress);
+
+    if (nameExists || addressExists) {
+      let message = '';
+      if (nameExists && addressExists) {
+        message = 'Le nom de contact et l\'adresse de contact existent déjà.';
+      } else if (nameExists) {
+        message = 'Le nom de contact existe déjà.';
+      } else if (addressExists) {
+        message = 'L\'adresse de contact existe déjà.';
+      }
+
+      const alert = await this.alertController.create({
+        header: 'Duplication trouvée',
+        message: message,
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    if (!this.newContactName || !this.newContactAddress) {
+      console.log('Missing required fields');
+      const alert = await this.alertController.create({
+        header: 'Champs obligatoires',
+        message: 'Le nom et l\'adresse du contact sont obligatoires.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    const newContact: ContactInterface = {
+      id: Date.now().toString(), // Génération d'un identifiant unique pour le contact
+      name: this.newContactName,
+      address: this.newContactAddress,
+      btcadress: this.newContactBtcWallet,
+      ethadress: this.newContactEthWallet,
+      usdtadress: this.newContactUsdtWallet,
+      bnbadress: this.newContactBnbWallet,
+      soladress: this.newContactSolWallet,
+      note: this.newContactNote
+    };
+    this.contactService.addContact(newContact);
+    this.newContactName = '';
+    this.newContactAddress = '';
+    this.newContactBtcWallet = '';
+    this.newContactEthWallet = '';
+    this.newContactUsdtWallet = '';
+    this.newContactBnbWallet = '';
+    this.newContactSolWallet = '';
+    this.newContactNote = '';
+
+    // Redirection vers la page des contacts lorsque le contact est ajouté
+    this.router.navigate(['/contacts-page']);
   }
-
 }
--- src/app/components/contacts-page/contacts-page.component.ts
+++ src/app/components/contacts-page/contacts-page.component.ts
@@ -1,61 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavigationMenuComponent } from "../navigation-menu/navigation-menu.component";
-import { WalletManagementService } from '../../services/wallet-management.service';
-import { ContactService, ContactInterface } from '../../services/contact.service';
-import { FormsModule } from '@angular/forms'; 
-import { IonicModule } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
-
+import { ContactService, ContactInterface } from '../../services/contact.service';
+import { AlertController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { NavigationMenuComponent } from "../navigation-menu/navigation-menu.component";
 
 @Component({
-    selector: 'app-contacts-page',
-    templateUrl: './contacts-page.component.html',
-    styleUrls: ['./contacts-page.component.scss'],
-    imports: [NavigationMenuComponent, FormsModule, CommonModule, IonicModule],
-    standalone: true,
+  selector: 'app-contacts-page',
+  templateUrl: './contacts-page.component.html',
+  styleUrls: ['./contacts-page.component.scss'],
+  imports: [CommonModule, FormsModule, IonicModule, NavigationMenuComponent],
+  standalone: true,
 })
 export class ContactsPageComponent implements OnInit {
-
   newContactName: string = '';
   newContactAddress: string = '';
   contacts: ContactInterface[] = [];
 
   constructor(
-    private walletService: WalletManagementService,
     private contactService: ContactService,
     private alertController: AlertController,
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
 
-  startChat(contactAdress: string): void {
-    this.router.navigate(['/talking-page', {address: contactAdress}]);
-  }
-
-  async ngOnInit() {
-    // Tentative de connexion au wallet
-    const wallet = await this.walletService.connectWithExistingAccount();
-    if (wallet) {
-      console.log('Connected to wallet', wallet);
-    } else {
-      console.error('Failed to connect to wallet');
-    }
-
-    // Chargement des contacts existants
+  ngOnInit() {
     this.loadContacts();
   }
 
-  navigateToAddContact(): void {
+  navigateToAddContact() {
     this.router.navigate(['/add-contact']);
   }
 
-  
+  async deleteContact(contactId: string, event: Event) {
+    event.stopPropagation();
 
-  async deleteContact(contactId: string): Promise<void> {
-    console.log('test');
-    console.log('Tentative de suppression du contact avec ID:', contactId); // Vérifiez si cet ID est correct
     const alert = await this.alertController.create({
       header: 'Confirmer la suppression',
       message: 'Voulez-vous vraiment supprimer ce contact ?',
@@ -63,29 +43,34 @@ export class ContactsPageComponent implements OnInit {
         {
           text: 'Annuler',
           role: 'cancel',
-          handler: () =>{
+          handler: () => {
             console.log('Suppression annulée');
           }
         }, {
           text: 'Supprimer',
           handler: () => {
-           this.contactService.deleteContact(contactId);
-           console.log('Contact supprimé');
+            this.contactService.deleteContact(contactId);
+            this.loadContacts();
+            console.log('Contact supprimé');
           }
         }
       ]
     });
     await alert.present();
-
   }
 
-  loadContacts(): void {
-    
+  loadContacts() {
     this.contacts = this.contactService.getContacts();
     console.log('Loaded contacts:', this.contacts);
   }
 
+  startChat(contactAddress: string) {
+    this.router.navigate(['/talking-page', { address: contactAddress }]);
+  }
 
-
-  
+  showInfo(contact: ContactInterface, event: Event) {
+    event.stopPropagation();
+    // Ajoutez ici la logique pour afficher des informations supplémentaires
+    console.log('Informations sur le contact:', contact);
+  }
 }
