5c902038d5d7954e5084e2625c635116b424d9af
hawoodrose <156238630+hawoodrose@users.noreply.github.com>
2024-06-05 11:57:17 +0200

    test

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    coingeckoApiKey: 'CG-4DK6WuCbbrS9uzmAX6xWMxfH',
+    coingeckoApiKey: 'test',
     NEWS_API_KEY:'c8b89a80c3ec4f1a85b10f931112cb23'
 }
